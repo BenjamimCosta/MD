51613f015d8796b840df1ed2248ceffafcfc7c22
Diogo Sambento <115667199+sambento7@users.noreply.github.com>
2023-03-10 10:46:17 +0000

    variance_threshold totalmente visto

--- Aula2/variance_threshold.py
+++ Aula2/variance_threshold.py
@@ -1,87 +1,49 @@
 import numpy as np
-from Aula1.dataset import Dataset
-
+#from Aula1.dataset import Dataset
+import sys
+sys.path.insert(1, 'D:/Universidade/MD/Aula1')
+from dataset import Dataset
 class VarianceThreshold:
-    """
-    Variance Threshold feature selection.
-    Features with a training-set variance lower than this threshold will be removed from the dataset.
+    '''
+    Classe que faz seleção/filtração de features de um Dataset utilizando um limite de variância
+    Todas as features cuja variância seja menor ou igual a este limite são eliminadas do Dataset
 
-    Parameters
-    ----------
-    threshold: float
-        The threshold value to use for feature selection. Features with a
-        training-set variance lower than this threshold will be removed.
-
-    Attributes
-    ----------
-    variance: array-like, shape (n_features,)
-        The variance of each feature.
-    """
+    Argumentos:
+        - threshold : float -> O valor limite usado para filtrar as features
+    Parâmetros estimados:
+        - variance : array de tamanho n (para n features) -> Guarda variância de cada feature do Dataset
+    '''
 
     def __init__(self, threshold: float = 0.0):
-        """
-        Variance Threshold feature selection.
-        Features with a training-set variance lower than this threshold will be removed from the dataset.
-
-        Parameters
-        ----------
-        threshold: float
-            The threshold value to use for feature selection. Features with a
-            training-set variance lower than this threshold will be removed.
-        """
         if threshold < 0:
-            raise ValueError("Threshold must be non-negative")
+            raise ValueError("Threshold deve ser não negativo")
 
-        # parameters
+        #Argumentos
         self.threshold = threshold
 
-        # attributes
+        #Parâemtros estimados
         self.variance = None
 
     def fit(self, dataset: Dataset) -> 'VarianceThreshold':
-        """
-        Fit the VarianceThreshold model according to the given training data.
-        Parameters
-        ----------
-        dataset : Dataset
-            The dataset to fit.
-
-        Returns
-        -------
-        self : object
-        """
+        #calcula variância de cada uma das features (por coluna)
         self.variance = np.var(dataset.X, axis=0)
         return self
 
     def transform(self, dataset: Dataset) -> Dataset:
-        """
-        It removes all features whose variance does not meet the threshold.
-        Parameters
-        ----------
-        dataset: Dataset
-
-        Returns
-        -------
-        dataset: Dataset
-        """
         X = dataset.X
 
-        features_mask = self.variance > self.threshold
-        X = X[:, features_mask]
-        features = np.array(dataset.features)[features_mask]
-        return Dataset(X=X, y=dataset.y, features=list(features), label=dataset.label)
+        #self.variance é um array com a variância de cada coluna, logo features_name_mask é um array que em cada posição tem um booleano que informa se aquela coluna 
+        #satisfaz a condição ou não
+        features_names_mask = self.variance > self.threshold
+        #Remove colunas que não satisfazem a condição, ou seja, que têm variância maior que o limite
+        X = X[:, features_names_mask]
+        #Remove features_names com este mesmo array de booleanos, tirando as mesmas colunas que foram removidas na linha acima
+        features_names = np.array(dataset.features_names)[features_names_mask]
+        #Retorna Dataset com as features filtradas de acordo com o limite de variância
+        return Dataset(X=X, y=dataset.y, features_names=list(features_names), label=dataset.label)
 
     def fit_transform(self, dataset: Dataset) -> Dataset:
-        """
-        Fit to data, then transform it.
-        Parameters
-        ----------
-        dataset: Dataset
-
-        Returns
-        -------
-        dataset: Dataset
-        """
+        #Dá fit dos dados (calcular vetor de variâncias) e depois transforma-os (filtra as features do dataset fornecido)
         self.fit(dataset)
         return self.transform(dataset)
 
@@ -91,10 +53,27 @@ if __name__ == '__main__':
                                   [0, 1, 4, 3],
                                   [0, 1, 1, 3]]),
                       y=np.array([0, 1, 0]),
-                      features=["f1", "f2", "f3", "f4"],
+                      features_names=["f1", "f2", "f3", "f4"],
                       label="y")
 
     selector = VarianceThreshold()
+    print("features names:")
+    print(dataset.features_names)
+    print("X:")
+    print(dataset.X)
+    print("Variância: " + str(selector.variance))
+    print("Limite: " + str(selector.threshold))
+    print("----------- fit --------------")
     selector = selector.fit(dataset)
+    print("features names:")
+    print(dataset.features_names)
+    print("X:")
+    print(dataset.X)
+    print("Variância: " + str(selector.variance))
+    print("----------- transform --------------")
     dataset = selector.transform(dataset)
-    print(dataset.features)
+    print("features names:")
+    print(dataset.features_names)
+    print("X:")
+    print(dataset.X)
+
